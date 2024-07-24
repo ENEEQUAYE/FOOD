8420478f5def69a45c7f8b284e8a510be6960965
EMMANUEL NEEQUAYE <eneequaye006@st.ug.edu.gh>
2024-07-24 01:46:28 +0000

    modified

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,50 @@
 import React from 'react'
-import { View, Text} from 'react-native'
+import { View,Image,TouchableOpacity, Text} from 'react-native'
 
 export default class Product extends React.Component{
     render(){
         return(
-            <View>
-                <Text>Product Component</Text>
-            </View>
+            <TouchableOpacity
+            onPress={this.props.onPress}
+            style={{
+                height: 280,
+                width: 160,
+                borderRadius: 20,
+                marginTop: this.props.marginTop,
+                backgroundColor: "#f5f5fa",
+                marginRight: 10,
+            }}
+            >
+              <View style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginTop: 20,
+                alignSelf: "center"
+              }}>
+                <Image
+                  source={require('../images/logo.jpg')}
+                  style={{ width: 25, height: 20 }} />
+                <Text style={{
+                    fontSize: 16,
+                    fontWeight: "bold",
+                    paddingHorizontal: 10,
+                }}>290 Calories</Text>
+                </View>
+                <Image
+                source={this.props.image}
+                style={{
+                    width: 130,
+                    height: 105,
+                    alignSelf: "center",
+                    marginTop: 15,
+                    marginBottom: 15,
+                }}/>  
+                <Text style={{
+                    fontSize: 18,
+                    fontWeight: "bold",
+                    paddingHorizontal: 10,
+                }}>{this.props.title}</Text>
+            </TouchableOpacity>
         )
     }
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,6 +3,7 @@ import { View, Image, Text } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 import { Picker } from '@react-native-picker/picker'
 import Icons from '@expo/vector-icons/Entypo'
+import Product from '../components/Product'
 
 export default class Home extends React.Component {
     state = {
@@ -28,7 +29,7 @@ export default class Home extends React.Component {
                     <View style={{ width: "80%" }}>
                         <Picker
                             selectedValue={this.state.city}
-                            style={{ height: 50, width: 160 }}
+                            style={{ height: 50, width: 160, marginLeft: 70 }}
                             onValueChange={(itemValue, itemIndex) =>
                                 this.setState({ city: itemValue })
                             }>
@@ -48,6 +49,152 @@ export default class Home extends React.Component {
                     <View>
                         <Icons name="magnifying-glass" size={30} />
                     </View>
+                </View>
+                <View style={{
+                    paddingHorizontal:20,
+                    marginTop: 20
+                }}>
+                    <Text style={{fontSize:30, fontWeight: "bold"}}>Food that</Text>
+                    <Text style={{fontSize:30, fontWeight: "bold"}}>meets your needs</Text>
+                </View>
+
+
+                <ScrollView 
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    style={{ marginTop: 40 }}>
+                    
+                    <View style={{
+                        alignItems: "center",
+                        flexDirection: "row",
+                        backgroundColor: "#f9dd7a",
+                        marginHorizontal: 15,
+                        borderRadius: 25,
+                        paddingVertical: 5,
+                        paddingHorizontal: 15
+                    }}>
+                        <Image
+                            source={require('../images/logo.jpg')}
+                            style={{ width: 55, height: 60 }}
+                        />
+                        <Text style={{
+                            paddingLeft: 10,
+                            fontWeight: "bold",
+                            fontSize: 15
+                        }}>Burgers</Text>
+                    </View>
+
+                    <View style={{
+                        alignItems: "center",
+                        flexDirection: "row",
+                        backgroundColor: "#f9dd7a",
+                        marginHorizontal: 15,
+                        borderRadius: 25,
+                        paddingVertical: 5,
+                        paddingHorizontal: 15
+                    }}>
+                        <Image
+                            source={require('../images/logo.jpg')}
+                            style={{ width: 55, height: 60 }}
+                        />
+                        <Text style={{
+                            paddingLeft: 10,
+                            fontWeight: "bold",
+                            fontSize: 15
+                        }}>Burgers</Text>
+                    </View>
+
+                    <View style={{
+                        alignItems: "center",
+                        flexDirection: "row",
+                        backgroundColor: "#f9dd7a",
+                        marginHorizontal: 15,
+                        borderRadius: 25,
+                        paddingVertical: 5,
+                        paddingHorizontal: 15
+                    }}>
+                        <Image
+                            source={require('../images/logo.jpg')}
+                            style={{ width: 55, height: 60 }}
+                        />
+                        <Text style={{
+                            paddingLeft: 10,
+                            fontWeight: "bold",
+                            fontSize: 15
+                        }}>Burgers</Text>
+                    </View>
+
+                    <View style={{
+                        alignItems: "center",
+                        flexDirection: "row",
+                        backgroundColor: "#f9dd7a",
+                        marginHorizontal: 15,
+                        borderRadius: 25,
+                        paddingVertical: 5,
+                        paddingHorizontal: 15
+                    }}>
+                        <Image
+                            source={require('../images/logo.jpg')}
+                            style={{ width: 55, height: 60 }}
+                        />
+                        <Text style={{
+                            paddingLeft: 10,
+                            fontWeight: "bold",
+                            fontSize: 15
+                        }}>Burgers</Text>
+                    </View>
+            </ScrollView>
+                <View style={{
+                    alignItems: "center",
+                    flexDirection: "row",
+                    marginTop: 40,
+                    marginHorizontal: 20,
+                }}>
+                    <View style={{width: "50%"}}>
+                        <Text style={{
+                            fontSize: 22,
+                            fontWeight: "bold",
+                        }}>New Products</Text>
+                    </View>
+                    <View style={{width: "50%", alignItems: "flex-end"}}>
+                        <Icons name="dots-three-horizontal" size={25} color= "#848385" />
+                        </View>
+                </View>
+                
+                <View style={{
+                    flexDirection: "row",
+                    marginHorizontal: 15,
+                    marginTop: 30,
+                }}>
+                    <Product
+                    image={require("../images/logo.jpg")}
+                        title="Smokehouse"
+                        price="12.99"
+                        onPress={() => this.props.navigation.navigate('Detail')}
+                    />
+                    <Product
+                    image={require("../images/logo.jpg")}
+                    title="Honey Chilli"
+                    price="10.99"
+                    marginTop={25}
+                    />
+                </View>
+                <View style={{
+                    flexDirection: "row",
+                    marginHorizontal: 15,
+                    marginTop: 30,
+                }}>
+                    <Product
+                    image={require("../images/logo.jpg")}
+                        title="Smokehouse"
+                        price="12.99"
+                    />
+                    <Product
+                    image={require("../images/logo.jpg")}
+                    title="Honey Chilli"
+                    price="10.99"
+                    marginTop={25}
+                    />
                 </View>
             </ScrollView>
         )
